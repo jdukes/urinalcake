5b95ecd6f101ab4b89bb1a03ecb2a6cea40c0d62
Josh Dukes <hex@neg9.org>
2014-02-07 21:39:14 -0800

    moved metaprogramming stuff to meta.py

--- urinalcake/native/__init__.py
+++ urinalcake/native/__init__.py
@@ -9,6 +9,7 @@ import os
 import inspect
 import functools
 
+from .meta import Live, advance
 
 #__all__ = []
 
@@ -410,42 +411,6 @@ def _detach(pid):
     ptrace(PTRACE_DETACH, pid, 0, 0)
 
 
-
-###############################################################################
-# Meta
-###############################################################################
-
-class Live(object):
-    """This descriptor allows attributes to determine if they have been
-modified.
-
-    This is useful for things like registers or file descriptors. On
-    set it adds
-
-    """
-    def __init__(self, name=None):
-        self.name = name
-        self.val = None
-    
-    def __get__(self, instance, objtype):
-        if self.name in instance._get_update:
-            instance._update_attr(self.name)
-        return self.val
-
-    def __set__(self, instance, val):
-        self.val = val
-        instance._set_update.add(self.name)
-
-
-def advance(fn):
-    #watch that video again and fix this
-    argspec = inspect.getargspec(fn)
-    @functools.wraps(fn)
-    def update_and_invalidate(cls, *args, **kwargs):
-        for attr in cls._invalidate_on_advance:
-            cls._invalidate_attr(attr)
-        return fn(cls, *args, **kwargs)
-    return update_and_invalidate
 
 
 ###############################################################################
